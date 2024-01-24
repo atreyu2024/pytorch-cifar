213d4693bdf2e0c114aec4440128db7bab4f9f19
atreyu <atreyu2024@proton.me>
2024-01-24 17:27:05 +0200

    update_resnet

--- models/resnet.py
+++ models/resnet.py
@@ -73,16 +73,21 @@ class Bottleneck(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
+        self._has_layer4 = (len(num_blocks) >= 4)
         self.in_planes = 64
-
+        cur_layers_planes = self.in_planes
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.layer1 = self._make_layer(block, cur_layers_planes, num_blocks[0], stride=1)
+        cur_layers_planes *= 2
+        self.layer2 = self._make_layer(block, cur_layers_planes, num_blocks[1], stride=2)
+        cur_layers_planes *= 2
+        self.layer3 = self._make_layer(block, cur_layers_planes, num_blocks[2], stride=2)
+        if self._has_layer4:
+            cur_layers_planes *= 2
+            self.layer4 = self._make_layer(block, cur_layers_planes, num_blocks[3], stride=2)
+        self.linear = nn.Linear(cur_layers_planes * block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -97,7 +102,8 @@ class ResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = self.layer4(out)
+        if self._has_layer4:
+            out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
@@ -107,6 +113,8 @@ class ResNet(nn.Module):
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
 
+def ResNet14():
+    return ResNet(BasicBlock, [2, 2, 2])
 
 def ResNet34():
     return ResNet(BasicBlock, [3, 4, 6, 3])
